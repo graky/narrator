1d852126a1fbaa0ccb1775a28f63e872c9383bbe
grakky <akimgrebenkin21@gmail.com>
2021-11-26 21:43:06 +0300

    village generation

--- generate_map.py
+++ generate_map.py
@@ -22,23 +22,57 @@ def generate_islands(map, island_number):
         x_cord = random.randint(0, 399)
         y_cord = random.randint(0, 399)
         base_texture = random.choice(["desert", "grass", "snow", "earth", "stone_texture"])
-        if x_cord+max_size > 399:
+        if x_cord+max_size*2 > 399:
             x_down = random.randint(0, 399-x_cord)
         else:
-            x_down = random.randint(0, max_size)
-        if x_cord-max_size < 0:
+            x_down = random.randint(0, max_size*2)
+        if x_cord-max_size*2 < 0:
             x_up = random.randint(0, x_cord)
         else:
-            x_up = random.randint(0, max_size)
+            x_up = random.randint(0, max_size*2)
         for i in range(x_cord-x_up, x_cord+x_down):
             row = map[i]
-            y_right = random.randint(0, max_size) if y_cord+max_size<399 else random.randint(0, 399-y_cord)
-            y_left = random.randint(0, max_size) if y_cord-max_size>0 else random.randint(0, y_cord)
+            y_right = random.randint(0, int(max_size/2)) if y_cord+int(max_size/2)<399 else random.randint(0, 399-y_cord)
+            y_left = random.randint(0, int(max_size/2)) if y_cord-int(max_size/2)>0 else random.randint(0, y_cord)
             for j in range(y_cord-y_left, y_cord+y_right):
                 row[j]["base_texture"] = base_texture
     return map
 
+def rand_cord_near_object(x_cord, y_cord, on_range, size):
+    on_range_x = on_range
+    on_range_x_minus = on_range
+    on_range_y = on_range
+    on_range_y_minus = on_range
+    if x_cord + on_range > size:
+        on_range_x = size - x_cord
+    if y_cord + on_range > size:
+        on_range_y = size - y_cord
+    if x_cord-on_range < 0:
+        on_range_x_minus = x_cord
+    if y_cord-on_range < 0:
+        on_range_y_minus = y_cord
+    x_plus = random.choice([False, True])
+    y_plus = random.choice([False, True])
+    if x_plus:
+        returned_x = x_cord + random.randint(0, on_range_x)
+    else:
+        returned_x = x_cord - random.randint(0, on_range_x_minus)
+    if y_plus:
+        returned_y = y_cord + random.randint(0, on_range_y)
+    else:
+        returned_y = y_cord - random.randint(0, on_range_y_minus)
+    return  returned_x, returned_y
 
+
+def generate_villagies(map, village_number):
+    for i in range(village_number):
+        x_cord = random.randint(0, 399)
+        y_cord = random.randint(0, 399)
+        map[x_cord][y_cord]["building"] = "farming_house"
+        for i in range(0, random.randint(0, 5)):
+            x_cord, y_cord = rand_cord_near_object(x_cord, y_cord, 5, 400)
+            map[x_cord][y_cord]["building"] = "farming_house"
+    return map
 
 
 
@@ -49,21 +83,12 @@ def generate_base_map():
         for el in row:
             el["base_texture"] = base_texture
     map_with_islands = generate_islands(map, 20)
-    return map_with_islands
-
-def plus_minus(number):
-    true_or_false = random.choice([True, False])
-    if true_or_false:
-        return number
-    return -number
-
-def rand_cord_near_object(x_cord, y_cord, on_range, size):
-    rand_x = x_cord + plus_minus(on_range)
-    rand_y = y_cord + plus_minus(on_range)
+    map_with_villagies = generate_villagies(map_with_islands, 4)
+    return map_with_villagies
 
 
 def generate_picture():
-    img = Image.new('RGB', (12800, 12800), color='red')
+    img = Image.new('RGB', (12800, 12800), color='white')
     map = generate_base_map()
     for i in range(0, 400, 2):
         row = map[i]
@@ -77,10 +102,28 @@ def generate_picture():
             img.paste(picture, ((i+1)*32, j*32))
             img.paste(picture, (i*32, (j+1)*32))
             img.paste(picture, ((i+1)*32, (j+1)*32))
+    for i in range(400):
+        row = map[i]
+        for j in range(400):
+            el = row[j]
+            if el.get("building") == "farming_house":
+                houses = "textures/houses"
+                item = random.choice(os.listdir(houses))
+                house = Image.open(f"{houses}/{item}")
+                house.resize((64, 64))
+                img.paste(house, (i*32, j*32))
 
-    img.save("full map generated6.jpg")
+    img.save("full map with village.jpg")
+    return map
 
 
+map = generate_picture()
 
-generate_picture()
-#generate_map()
+i = 0
+
+for row in map:
+    for el in row:
+        if el.get("building" ) ==   "farming_house":
+            i += 1
+
+print(i)
